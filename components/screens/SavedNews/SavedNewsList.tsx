f4e52b096c8428673fbb5a96e42089c96bf7a855
hun <skyyy5347@naver.com>
2025-11-06 22:09:19 +0900

    기사 앞/뒤 이동 추가

--- components/screens/SavedNews/SavedNewsList.tsx
+++ components/screens/SavedNews/SavedNewsList.tsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
-import { ScrollView, Text, View } from 'react-native'
-import { SavedNewsListProps } from '../../../types/screens'
-import { SavedNewsCard } from './SavedNewsCard'
+import { ScrollView, Text, View } from 'react-native';
+import { SavedNewsListProps } from '../../../types/screens';
+import { SavedNewsCard } from './SavedNewsCard';
 
 export const SavedNewsList = memo(function SavedNewsList({
   newsList,
--- components/screens/SavedNews/index.ts
+++ components/screens/SavedNews/index.ts
@@ -1,3 +1,4 @@
 export { SavedNewsCard } from './SavedNewsCard'
 export { SavedNewsList } from './SavedNewsList'
 export { SavedNewsScreen } from './SavedNewsScreen'
+
