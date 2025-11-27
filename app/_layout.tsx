89fb375a2433e78ddb2c0997ddfb60127bb469d5
JINI0617 <seojin8476@naver.com>
2025-11-27 13:20:57 +0900

    Fix: publicRoutes 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout() {
     "/ResetPasswordPage",
     "/SelectCategoryPage",
     "/KakaoLoginView",
+    "/SelectGenderPage", 
+    "/SelectAgePage",
+    "/SelectTimePage"
   ];
 
   useEffect(() => {
